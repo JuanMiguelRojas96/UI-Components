6b108be1c896f6025c16b71fe78ff1f8eba7f05a
Juan Miguel Rojas Noriega <juan.rojas@pragma.com.co>
2025-02-23 18:30:31 -0500

    Se retiran las constantes

--- utils/color.styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from "@emotion/react";
-
-export const colorStyles = css`
-  :root {
-    --orange-primary: #EA7C69;
-    --color-secondary: #252836;
-    --color-tertiary: #3b3a4a;
-    }
-`;
